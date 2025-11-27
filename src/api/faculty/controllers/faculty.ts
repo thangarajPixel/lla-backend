8b06ed0642ddff6d65edcfa756b7e6cb5129a3fa
panneer11 <panneerselvam.t@capecomsolutions.com>
2025-11-27 10:27:50 +0530

    changes

--- src/api/faculty/controllers/faculty.ts
+++ src/api/faculty/controllers/faculty.ts
@@ -4,4 +4,40 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::faculty.faculty');
+export default factories.createCoreController(
+  "api::faculty.faculty",
+  ({ strapi }) => ({
+    async find(ctx) {
+      try {
+        const entity = await strapi.db.query("api::faculty.faculty").findOne({
+          populate: {
+            Faculty: {
+              on: {
+                "faculty.faculty": {
+                  populate: {
+                    Image: {
+                      select: ["id", "name", "url"],
+                    },
+                  },
+                }
+              },
+            },
+          },
+        });
+
+        if (!entity) {
+          return ctx.notFound("Faculty content not found");
+        }
+
+        // Add base URL to media URLs
+        // addBaseUrlToMediaUrls(entity);
+
+        const sanitizedEntity = await super.sanitizeOutput(entity, ctx);
+        return super.transformResponse(sanitizedEntity);
+      } catch (error) {
+        console.error("Home find error:", error);
+        return ctx.internalServerError("Failed to load home data");
+      }
+    },
+  })
+);
