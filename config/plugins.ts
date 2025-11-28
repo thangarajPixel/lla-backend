feaae194b17bc84dccfab10968445b8a27e4919d
ajithmv <ajith.mv@capecomsolutions.com>
2025-11-28 10:50:32 +0530

    changes

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,21 @@
 module.exports = {
-  // Example plugin configuration
-}
+  documentation: {
+    enabled: true,
+    config: {
+      info: {
+        title: "API Documentation",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: "https://dev-admin.lightandlifeacademy.in",
+          description: "Live Server",
+        },
+        {
+          url: "http://localhost:8000",
+          description: "Local Server",
+        }
+      ]
+    },
+  },
+};
