c7b51ee186c969af727d0d3e47d552287804085c
panneer11 <panneerselvam.t@capecomsolutions.com>
2025-12-12 17:49:45 +0530

    changes

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -204,6 +204,13 @@ export interface CourseBanner extends Struct.ComponentSchema {
   };
   attributes: {
     Btn_txt: Schema.Attribute.String;
+    Description: Schema.Attribute.RichText &
+      Schema.Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'defaultHtml';
+        }
+      >;
     Duration: Schema.Attribute.String;
     Image: Schema.Attribute.Media<
       'images' | 'files' | 'videos' | 'audios',
