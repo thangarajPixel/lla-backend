8e0bc85548cd4d41f596f6eb76edac8d45413833
manikandan <manikandan@capecomsolutions.com>
2026-01-09 23:57:52 +0530

    changes

--- src/api/state/controllers/state.ts
+++ src/api/state/controllers/state.ts
@@ -4,4 +4,20 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::state.state');
+export default factories.createCoreController('api::state.state', ({ strapi }) => ({
+  async find(ctx) {
+    // Add sort parameter to query
+    ctx.query = {
+      ...ctx.query,
+      sort: 'name:asc',
+    };
+    
+    const { data } = await super.find(ctx);
+    const modifiedData = data.map((state) => ({
+      id: state.id,
+      documentId: state.documentId,
+      name: state.name,
+    }));
+    return { data: modifiedData };
+  },
+}));
